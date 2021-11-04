ed81bcd523a698b372c945cc9061d955fe7e1b68
EvelynGriffith <70599987+EvelynGriffith@users.noreply.github.com>
2021-11-04 17:30:58 -0400

    fixing flake8

--- containmentcheck/containmentcheck/main.py
+++ containmentcheck/containmentcheck/main.py
@@ -71,9 +71,7 @@ def generate_random_container(
     # convert the list to a tuple
     # only when the make_tuple boolean variable is True
     # return the randomly generated container of values
-    randomlist: List[
-        Any,
-    ] = []
+    randomlist: List[any, ] = []
     while len(randomlist) < size:
         n = random.randint(0, maximum)
         randomlist.append(n)
